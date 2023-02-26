0b08526758fec59c018339d1915391e759e1cec9
JasonCaoAU <jason2019au@gmail.com>
2023-02-26 19:40:38 +1100

    add jenkins deploy app to ec2 server logic

--- script.groovy
+++ script.groovy
@@ -17,7 +17,8 @@ def deployApp() {
     def dockerRunAppCMD = 'docker run -p 8080:8080 -d --name java-app jason8746/my-app:1.0.0-java-multi-pipeline-amd64'
     sshagent(['ec2-server']) {
         sh "ssh -o StrictHostkeyChecking=no ec2-user@3.25.180.251 ${dockerRunAppCMD}"
-} 
+    } 
+}
 
 def masterBranch(){
     BRANCH_NAME == "master"
