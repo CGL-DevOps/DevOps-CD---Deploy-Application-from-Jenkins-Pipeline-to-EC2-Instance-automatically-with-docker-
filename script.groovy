8e6ac559d5ec97e41a34c20c2200997be0242050
Jason Cao <85828920+CGL-DevOps@users.noreply.github.com>
2023-02-26 22:00:11 +1100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -14,7 +14,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    def dockerRunAppCMD = 'docker run -p 8080:8080 -d --name java-app jason8746/my-app:1.0.0-java-multi-pipeline-amd64'
+    def dockerRunAppCMD = 'docker run -p 8080:8080 -d jason8746/my-app:1.0.0-java-multi-pipeline-amd64'
     sshagent(['ec2-server']) {
         sh "ssh -o StrictHostkeyChecking=no ec2-user@3.25.180.251 ${dockerRunAppCMD}"
     } 
